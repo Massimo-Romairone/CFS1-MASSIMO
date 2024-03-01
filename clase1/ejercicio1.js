ba2ed7f662fde0bcc2b8ec0176c3e54d38788271
Massimo-Romairone <mopvladd@gmail.com>
2024-02-29 21:26:35 -0300

    Mi primer commit

---
+++ clase1/ejercicio1.js
@@ -0,0 +1,11 @@
+console.log("Dirigirse desde su domicilio hasta el supermercado");
+console.log("Vestirse y agarrar las llaves del auto");
+console.log("Salir afuera, abrir el auto y subirse");
+console.log("Arrancar el auto");
+console.log("Avanzar 100 metros y doblar a la derecha");
+console.log("Frenar en el semaforo");
+console.log("avanzar otros 100 metros");
+console.log("Doblar a la izquierda");
+console.log("Entrar al estacionamiento y estacionarse");
+console.log("Bajarse del auto y ponerle llave");
+console.log("Entrar al supermercado");
